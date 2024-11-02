539ec8f1a6816267344a5de2845b504430188f1e
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-02 09:47:31 +0800

    add select picker

---
+++ src/lib/components/general/SelectPicker.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import * as Select from '$lib/components/ui/select/index.js';
+  import { cn } from '$lib/utils';
+  import type { ClassNameValue } from 'tailwind-merge';
+
+  interface Props {
+    props: {
+      name: string;
+      id: string;
+      'data-fs-error': string | undefined;
+      'aria-describedby': string | undefined;
+      'aria-invalid': 'true' | undefined;
+      'aria-required': 'true' | undefined;
+      'data-fs-control': string;
+    };
+    selections: {
+      label: string;
+      value: string;
+    }[];
+    selected: string;
+    class: ClassNameValue;
+  }
+
+  let { selected = $bindable(), ...props }: Props = $props();
+</script>
+
+<Select.Root type="single" bind:value={selected}>
+  <Select.Trigger class={cn('', props.class)}></Select.Trigger>
+  <Select.Content>
+    {#each props.selections as selection}
+      <Select.Item value={selection.value}>{selection.label}</Select.Item>
+    {/each}
+  </Select.Content>
+</Select.Root>
