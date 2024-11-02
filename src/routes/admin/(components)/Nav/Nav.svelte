68a9109a1c44b7e4bf79df3778f95b2b5df6310f
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-02 20:35:02 +0800

    update folder structure

---
+++ src/routes/admin/(components)/Nav/Nav.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Logout from './components/Logout.svelte';
+</script>
+
+<nav class="sticky top-0 z-50 bg-gradient-to-l from-[#3331C2] to-black p-2">
+  <div class="container flex items-center justify-between">
+    <div class="flex items-center gap-2">
+      <img src="/favicon.png" alt="system logo" class="h-[50px] w-[50px]" />
+      <span class="text-2xl font-bold text-white">ProfSched</span>
+    </div>
+
+    <div class="flex items-center gap-2">
+      <Logout />
+    </div>
+  </div>
+</nav>
--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import Nav from './(components)/Nav/Nav.svelte';
+
   const { children } = $props();
 </script>
 
+<Nav />
 {@render children()}
