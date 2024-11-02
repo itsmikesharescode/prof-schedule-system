3a34f05a42b26a8475be9f2fa050cf3fc1e498f6
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-03 00:45:47 +0800

    update update prof UI

--- src/routes/admin/accounts/components/UpdateProfessor/UpdateProfessor.svelte
+++ src/routes/admin/accounts/components/UpdateProfessor/UpdateProfessor.svelte
@@ -2,11 +2,18 @@
   import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
   import Button from '$lib/components/ui/button/button.svelte';
   import { X, Plus } from 'lucide-svelte';
-  import { type Infer, superForm, type SuperValidated } from 'sveltekit-superforms';
+  import { fileProxy, type Infer, superForm, type SuperValidated } from 'sveltekit-superforms';
   import { zodClient } from 'sveltekit-superforms/adapters';
   import * as Form from '$lib/components/ui/form/index.js';
   import { Input } from '$lib/components/ui/input/index.js';
   import { updateProfessorSchema, type UpdateProfessorSchema } from './schema';
+  import ImagePicker from '$lib/components/general/ImagePicker.svelte';
+  import { ScrollArea } from '$lib/components/ui/scroll-area/index';
+  import Combobox from '$lib/components/general/Combobox.svelte';
+  import { availableTimes, days, departments, interests } from '$lib/metadata';
+  import SelectPicker from '$lib/components/general/SelectPicker.svelte';
+  import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
+
   interface Props {
     updateProfessorForm: SuperValidated<Infer<UpdateProfessorSchema>>;
     showUpdate: boolean;
@@ -20,15 +27,25 @@
 
   const { form: formData, enhance, submitting } = form;
 
+  const file = fileProxy(form, 'photo');
+
+  const addInterest = (id: string) => {
+    $formData.interests = [...$formData.interests, id];
+  };
+
+  const removeInterest = (id: string) => {
+    $formData.interests = $formData.interests.filter((i) => i !== id);
+  };
+
   $effect(() => {
     if (showUpdate) {
-      //populate the id of target
+      //populate id to update
     }
   });
 </script>
 
 <AlertDialog.Root bind:open={showUpdate}>
-  <AlertDialog.Content>
+  <AlertDialog.Content class="max-w-7xl p-0">
     <button
       onclick={() => {
         showUpdate = false;
@@ -40,61 +57,283 @@
       <span class="sr-only">Close</span>
     </button>
 
-    <AlertDialog.Header>
+    <AlertDialog.Header class="px-6 pt-6">
       <AlertDialog.Title>Update Professor</AlertDialog.Title>
       <AlertDialog.Description>
-        Fill the fields below to update the professor.
+        Fill the fields below to update a professor.
       </AlertDialog.Description>
     </AlertDialog.Header>
+    <ScrollArea class="h-[80dvh]">
+      <form method="POST" enctype="multipart/form-data" use:enhance class=" ">
+        <div class="grid grid-cols-3 gap-6 px-6 pb-6">
+          <!--Personal Details-->
+          <div class="">
+            <div class="mb-6">
+              <span class="font-semibold text-muted-foreground underline">Personal Details</span>
+            </div>
+            <Form.Field {form} name="photo">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Photo</Form.Label>
+                  <ImagePicker bind:imageLink={$formData.photo} />
+                  <input class="hidden" type="file" {...props} bind:files={$file} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
 
-    <form method="POST" use:enhance>
-      <Form.Field {form} name="programHead">
-        <Form.Control>
-          {#snippet children({ props })}
-            <Form.Label>Program Head</Form.Label>
-            <Input
-              {...props}
-              bind:value={$formData.programHead}
-              placeholder="Enter program head name"
-            />
-          {/snippet}
-        </Form.Control>
-        <Form.Description />
-        <Form.FieldErrors />
-      </Form.Field>
+            <Form.Field {form} name="title">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Title</Form.Label>
+                  <Input {...props} bind:value={$formData.title} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
 
-      <Form.Field {form} name="department">
-        <Form.Control>
-          {#snippet children({ props })}
-            <Form.Label>Department Code</Form.Label>
-            <Input
-              {...props}
-              bind:value={$formData.department}
-              placeholder="Enter department code"
-            />
-          {/snippet}
-        </Form.Control>
-        <Form.Description />
-        <Form.FieldErrors />
-      </Form.Field>
+            <Form.Field {form} name="firstName">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>First Name</Form.Label>
+                  <Input {...props} bind:value={$formData.firstName} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
 
-      <Form.Field {form} name="description">
-        <Form.Control>
-          {#snippet children({ props })}
-            <Form.Label>Description</Form.Label>
-            <Input
-              {...props}
-              bind:value={$formData.description}
-              placeholder="Enter program description"
-            />
-          {/snippet}
-        </Form.Control>
-        <Form.Description />
-        <Form.FieldErrors />
-      </Form.Field>
-      <AlertDialog.Footer>
-        <Form.Button size="sm">Update</Form.Button>
-      </AlertDialog.Footer>
-    </form>
+            <Form.Field {form} name="middleName">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Middle Name</Form.Label>
+                  <Input {...props} bind:value={$formData.middleName} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="lastName">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Last Name</Form.Label>
+                  <Input {...props} bind:value={$formData.lastName} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="password">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Password</Form.Label>
+                  <Input {...props} bind:value={$formData.password} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="confirmPassword">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Input {...props} bind:value={$formData.confirmPassword} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
+          </div>
+
+          <!--Academic Details-->
+          <div class="">
+            <div class="mb-6">
+              <span class="font-semibold text-muted-foreground underline">Academic Details</span>
+            </div>
+            <Form.Field {form} name="previousSchool">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Previous School</Form.Label>
+                  <Input
+                    {...props}
+                    bind:value={$formData.previousSchool}
+                    placeholder="Enter your previous school"
+                  />
+                {/snippet}
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="yearsOfTeaching">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Years of Teaching</Form.Label>
+                  <Input
+                    type="number"
+                    {...props}
+                    bind:value={$formData.yearsOfTeaching}
+                    placeholder="Enter your years of teaching"
+                  />
+                {/snippet}
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="department">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Department</Form.Label>
+                  <SelectPicker
+                    name="Select department"
+                    {props}
+                    class=""
+                    selections={departments}
+                    bind:selected={$formData.department}
+                  />
+                  <input type="hidden" {...props} bind:value={$formData.department} />
+                {/snippet}
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="day">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Day</Form.Label>
+                  <SelectPicker
+                    name="Select day"
+                    {props}
+                    class=""
+                    selections={days}
+                    bind:selected={$formData.day}
+                  />
+                  <input type="hidden" {...props} bind:value={$formData.day} />
+                {/snippet}
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="startTime">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Start Time</Form.Label>
+                  <Combobox
+                    name="Select start time"
+                    placeholder="Select start time"
+                    selections={availableTimes}
+                    bind:selected={$formData.startTime}
+                  />
+                  <input type="hidden" {...props} bind:value={$formData.startTime} />
+                {/snippet}
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="endTime">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>End Time</Form.Label>
+                  <Combobox
+                    name="Select end time"
+                    placeholder="Select end time"
+                    selections={availableTimes}
+                    bind:selected={$formData.endTime}
+                  />
+                  <input type="hidden" {...props} bind:value={$formData.endTime} />
+                {/snippet}
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+
+            <Form.Field {form} name="availability">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Availability</Form.Label>
+                  <SelectPicker
+                    name="Select availability"
+                    {props}
+                    class=""
+                    selections={[
+                      { value: 'Part Time', label: 'Part Time' },
+                      { value: 'Full Time', label: 'Full Time' }
+                    ]}
+                    bind:selected={$formData.availability}
+                  />
+                  <input type="hidden" {...props} bind:value={$formData.availability} />
+                {/snippet}
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+          </div>
+
+          <!--Interest-->
+          <div class="">
+            <div class="mb-6">
+              <span class="font-semibold text-muted-foreground underline">Interest</span>
+            </div>
+
+            <Form.Fieldset {form} name="interests">
+              <div class="space-y-2">
+                {#each interests as interest}
+                  {@const checked = $formData.interests.includes(interest.value)}
+                  <div class="flex flex-row items-start space-x-3">
+                    <Form.Control>
+                      {#snippet children({ props })}
+                        <Checkbox
+                          {...props}
+                          {checked}
+                          value={interest.value}
+                          onCheckedChange={(v) => {
+                            if (v) {
+                              addInterest(interest.value);
+                            } else {
+                              removeInterest(interest.value);
+                            }
+                          }}
+                        />
+                        <Form.Label class="font-normal">
+                          {interest.label}
+                        </Form.Label>
+                      {/snippet}
+                    </Form.Control>
+                  </div>
+                {/each}
+                <Form.FieldErrors />
+              </div>
+            </Form.Fieldset>
+
+            <Form.Field {form} name="position">
+              <Form.Control>
+                {#snippet children({ props })}
+                  <Form.Label>Position</Form.Label>
+                  <SelectPicker
+                    name="Select position"
+                    {props}
+                    class=""
+                    selections={[
+                      { value: 'Professor', label: 'Professor' },
+                      { value: 'Program Head', label: 'Program Head' }
+                    ]}
+                    bind:selected={$formData.position}
+                  />
+                  <input type="hidden" {...props} bind:value={$formData.position} />
+                {/snippet}
+              </Form.Control>
+              <Form.Description />
+              <Form.FieldErrors />
+            </Form.Field>
+          </div>
+        </div>
+
+        <div class="pointer-events-none sticky bottom-6 left-0 right-0 flex justify-end px-6">
+          <Form.Button size="sm" class="pointer-events-auto">Update</Form.Button>
+        </div>
+      </form>
+    </ScrollArea>
   </AlertDialog.Content>
 </AlertDialog.Root>
