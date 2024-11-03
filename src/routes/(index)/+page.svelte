aecceea1107748ae3fc60d8d8b5e51be11094ded
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-03 12:05:08 +0800

    update nav

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Nav from './components/Nav/Nav.svelte';
 </script>
 
+<Nav />
 <main></main>
--- src/routes/(index)/components/Nav/Nav.svelte
+++ src/routes/(index)/components/Nav/Nav.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+  import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<nav></nav>
+<nav class="w-full border-b-2 bg-secondary">
+  <div class="container flex items-center justify-between p-2">
+    <div class="flex items-center gap-2">
+      <img src="/favicon.png" alt="system logo" class="h-10 w-10" />
+      <span class="text-xl font-bold">ProfSched</span>
+    </div>
+
+    <div class="">
+      <Button size="sm">Log in</Button>
+      <Button size="sm">Sign Up Free</Button>
+    </div>
+  </div>
+</nav>
