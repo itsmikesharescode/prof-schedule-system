03cda0af72a0296a8159c8cd21d1bf7234db37b2
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-03 10:33:12 +0800

    add rooms page UI

---
+++ src/routes/admin/(records)/rooms/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import * as Table from '$lib/components/ui/table/index.js';
+  import AddRoom from './components/AddRoom/AddRoom.svelte';
+  import TableMenu from './components/TableMenu.svelte';
+
+  const { data } = $props();
+</script>
+
+<div class="flex flex-col gap-4">
+  <div class="sticky top-2 z-30 flex justify-end">
+    <AddRoom addRoomForm={data.addRoomForm} />
+  </div>
+
+  <Table.Root>
+    <Table.Caption>A list of your recent invoices.</Table.Caption>
+    <Table.Header>
+      <Table.Row>
+        <Table.Head class="w-[50px]"></Table.Head>
+        <Table.Head class="w-[100px] truncate">Room Type</Table.Head>
+        <Table.Head class="w-[100px] truncate">Room Number</Table.Head>
+        <Table.Head class="w-[100px] truncate">Room Code</Table.Head>
+        <Table.Head class="text-right">Created At</Table.Head>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {#each Array(20) as _}
+        <Table.Row>
+          <Table.Cell class="">
+            <TableMenu updateRoomForm={data.updateRoomForm} />
+          </Table.Cell>
+          <Table.Cell class="truncate font-medium">Lecture Room</Table.Cell>
+          <Table.Cell class="">2</Table.Cell>
+          <Table.Cell class="">Comlab2</Table.Cell>
+          <Table.Cell class="text-right">{new Date().toLocaleDateString()}</Table.Cell>
+        </Table.Row>
+      {/each}
+    </Table.Body>
+  </Table.Root>
+</div>
---
+++ src/routes/admin/(records)/rooms/components/AddRoom/AddRoom.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+  import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+  import Button from '$lib/components/ui/button/button.svelte';
+  import { X, Plus } from 'lucide-svelte';
+  import { type Infer, superForm, type SuperValidated } from 'sveltekit-superforms';
+  import { zodClient } from 'sveltekit-superforms/adapters';
+  import * as Form from '$lib/components/ui/form/index.js';
+  import { Input } from '$lib/components/ui/input/index.js';
+  import { addRoomSchema, type AddRoomSchema } from './schema';
+
+  interface Props {
+    addRoomForm: SuperValidated<Infer<AddRoomSchema>>;
+  }
+
+  let { addRoomForm }: Props = $props();
+
+  let open = $state(false);
+
+  const form = superForm(addRoomForm, {
+    validators: zodClient(addRoomSchema)
+  });
+
+  const { form: formData, enhance, submitting } = form;
+</script>
+
+<Button size="sm" onclick={() => (open = true)}>
+  <Plus class="size-4" />
+  Add Room
+</Button>
+
+<AlertDialog.Root bind:open>
+  <AlertDialog.Content class="">
+    <button
+      onclick={() => {
+        open = false;
+        form.reset();
+      }}
+      class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
+    >
+      <X class="size-4" />
+      <span class="sr-only">Close</span>
+    </button>
+
+    <AlertDialog.Header class="">
+      <AlertDialog.Title>New Room</AlertDialog.Title>
+      <AlertDialog.Description>Fill the fields below to create a new room.</AlertDialog.Description>
+    </AlertDialog.Header>
+
+    <form method="POST" use:enhance>
+      <Form.Field {form} name="roomType">
+        <Form.Control>
+          {#snippet children({ props })}
+            <Form.Label>Room Type</Form.Label>
+            <Input {...props} bind:value={$formData.roomType} placeholder="Enter room type" />
+          {/snippet}
+        </Form.Control>
+        <Form.Description />
+        <Form.FieldErrors />
+      </Form.Field>
+
+      <Form.Field {form} name="roomNumber">
+        <Form.Control>
+          {#snippet children({ props })}
+            <Form.Label>Room Number</Form.Label>
+            <Input {...props} bind:value={$formData.roomNumber} placeholder="Enter room number" />
+          {/snippet}
+        </Form.Control>
+        <Form.Description />
+        <Form.FieldErrors />
+      </Form.Field>
+
+      <Form.Field {form} name="roomCode">
+        <Form.Control>
+          {#snippet children({ props })}
+            <Form.Label>Room Code</Form.Label>
+            <Input
+              type="number"
+              {...props}
+              bind:value={$formData.roomCode}
+              placeholder="Enter room code"
+            />
+          {/snippet}
+        </Form.Control>
+        <Form.Description />
+        <Form.FieldErrors />
+      </Form.Field>
+
+      <AlertDialog.Footer>
+        <Form.Button size="sm">Create</Form.Button>
+      </AlertDialog.Footer>
+    </form>
+  </AlertDialog.Content>
+</AlertDialog.Root>
