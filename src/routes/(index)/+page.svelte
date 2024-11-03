07189adbdad68396a829cb1cc0cd3fb4dff2ee4f
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-03 12:11:49 +0800

    update landing

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -3,4 +3,28 @@
 </script>
 
 <Nav />
-<main></main>
+<main class="min-h-screen">
+  <div class="container mt-5 grid grid-cols-2">
+    <div class="flex max-w-3xl flex-col gap-2">
+      <span class="text-3xl font-semibold">WHY PROFSCHED?</span>
+      <p class="text-muted-foreground">
+        Manually creating schedules has several issues because it takes a lot of time and is easy to
+        mess up. One Cainta College faces issues with setting class schedules and managing faculty
+        loads, leading to inconsistencies that hinder productivity and cause delays. One Cainta
+        College is an educational institution that, like many others, relies heavily on effective
+        schedule management.
+      </p>
+
+      <p class="text-muted-foreground">
+        This process is crucial for the smooth functioning of the academic ecosystem, ensuring that
+        both professors and students can plan their time effectively and avoid disruptions.
+      </p>
+
+      <p class="text-muted-foreground">
+        Manual scheduling prevalent in many institutions poses drawbacks due to the potential for
+        human error and the time-consuming nature of the process. Even minor mistakes can disrupt
+        the entire academic ecosystem, impacting both professors' and students' productivity.
+      </p>
+    </div>
+  </div>
+</main>
