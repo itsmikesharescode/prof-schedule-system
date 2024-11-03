b782ae4c8dd211179c08718b2670b6be28add6ed
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-03 09:09:44 +0800

    update correct btn for sidebar

--- src/routes/admin/(components)/Nav/components/Profile.svelte
+++ src/routes/admin/(components)/Nav/components/Profile.svelte
@@ -1,35 +1,36 @@
 <script lang="ts">
-  import * as Popover from '$lib/components/ui/popover/index.js';
   import * as Avatar from '$lib/components/ui/avatar/index.js';
   import { Settings } from 'lucide-svelte';
   import Logout from './Logout.svelte';
-  import Button from '$lib/components/ui/button/button.svelte';
-
-  //todo update this to dropdown menu
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+  import { buttonVariants } from '$lib/components/ui/button';
 </script>
 
-<Popover.Root>
-  <Popover.Trigger class="w-full">
-    <Button size="sm" class="flex w-full items-center justify-start gap-2">
+<DropdownMenu.Root>
+  <DropdownMenu.Trigger class={buttonVariants({ variant: 'default', size: 'sm' })}>
+    <div class="flex w-full items-center gap-2.5">
       <Settings />
       Profile
-    </Button>
-  </Popover.Trigger>
-
-  <Popover.Content class="flex flex-col gap-4">
-    <div class="flex items-center gap-2">
-      <Avatar.Root>
-        <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-        <Avatar.Fallback>CN</Avatar.Fallback>
-      </Avatar.Root>
-      <div class="flex flex-col">
-        <span class="line-clamp-1 text-sm font-semibold">Mike John Eviota</span>
-        <span class="line-clamp-1 text-sm text-gray-500">mike@eviota.com</span>
-      </div>
     </div>
-
-    <div class="flex justify-end">
-      <Logout />
-    </div>
-  </Popover.Content>
-</Popover.Root>
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content class="w-[300px]">
+    <DropdownMenu.Group>
+      <DropdownMenu.GroupHeading>
+        <div class="flex items-center gap-2">
+          <Avatar.Root>
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+          </Avatar.Root>
+          <div class="flex flex-col">
+            <span class="line-clamp-1 text-sm font-semibold">Mike John Eviota</span>
+            <span class="line-clamp-1 text-sm text-gray-500">mike@eviota.com</span>
+          </div>
+        </div>
+        <DropdownMenu.Separator />
+        <div class="flex justify-end">
+          <Logout />
+        </div>
+      </DropdownMenu.GroupHeading>
+    </DropdownMenu.Group>
+  </DropdownMenu.Content>
+</DropdownMenu.Root>
