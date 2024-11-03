7a5f1cc1484f00c1fccc6c8ef8cecb5164ce8294
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-03 12:23:31 +0800

    update landing page

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -4,10 +4,12 @@
 
 <Nav />
 <main class="min-h-screen">
-  <div class="container mt-5 grid grid-cols-2">
-    <div class="flex max-w-3xl flex-col gap-2">
+  <div class="container mt-5 flex flex-col gap-5">
+    <div
+      class="mx-auto flex max-w-3xl flex-col items-center justify-center gap-5 rounded-lg bg-secondary/50 p-10 shadow-lg"
+    >
       <span class="text-3xl font-semibold">WHY PROFSCHED?</span>
-      <p class="text-muted-foreground">
+      <p class="text-center text-muted-foreground">
         Manually creating schedules has several issues because it takes a lot of time and is easy to
         mess up. One Cainta College faces issues with setting class schedules and managing faculty
         loads, leading to inconsistencies that hinder productivity and cause delays. One Cainta
@@ -15,16 +17,31 @@
         schedule management.
       </p>
 
-      <p class="text-muted-foreground">
+      <p class="text-center text-muted-foreground">
         This process is crucial for the smooth functioning of the academic ecosystem, ensuring that
         both professors and students can plan their time effectively and avoid disruptions.
       </p>
 
-      <p class="text-muted-foreground">
+      <p class="text-center text-muted-foreground">
         Manual scheduling prevalent in many institutions poses drawbacks due to the potential for
         human error and the time-consuming nature of the process. Even minor mistakes can disrupt
         the entire academic ecosystem, impacting both professors' and students' productivity.
       </p>
+    </div>
+
+    <div class="flex items-center justify-center gap-10">
+      <div class="flex flex-col items-center justify-center gap-2.5">
+        <div class="h-[200px] w-[200px] rounded-full bg-secondary"></div>
+        <span class="text-xl font-bold">Beneficiaries</span>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-2.5">
+        <div class="h-[200px] w-[200px] rounded-full bg-secondary"></div>
+        <span class="text-xl font-bold">Scheduler</span>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-2.5">
+        <div class="h-[200px] w-[200px] rounded-full bg-secondary"></div>
+        <span class="text-xl font-bold">Researchers</span>
+      </div>
     </div>
   </div>
 </main>
--- src/routes/(index)/components/Nav/Nav.svelte
+++ src/routes/(index)/components/Nav/Nav.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+  import Darkmode from '$lib/components/general/Darkmode.svelte';
   import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<nav class="w-full border-b-2 bg-secondary">
+<nav class="sticky top-0 z-50 w-full border-b-2 bg-secondary">
   <div class="container flex items-center justify-between p-2">
     <div class="flex items-center gap-2">
       <img src="/favicon.png" alt="system logo" class="h-10 w-10" />
       <span class="text-xl font-bold">ProfSched</span>
     </div>
 
-    <div class="">
+    <div class="flex items-center gap-2.5">
       <Button size="sm">Log in</Button>
       <Button size="sm">Sign Up Free</Button>
+      <Darkmode />
     </div>
   </div>
 </nav>
