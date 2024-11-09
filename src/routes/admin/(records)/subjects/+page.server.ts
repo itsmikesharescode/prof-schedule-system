afc5193856b21694c671506df75a332bd9d48f2d
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-09 17:18:01 +0800

    add: add subject funcionality

--- src/routes/admin/(records)/subjects/+page.server.ts
+++ src/routes/admin/(records)/subjects/+page.server.ts
@@ -17,10 +17,21 @@ export const load: PageServerLoad = async () => {
 export const actions: Actions = {
   addSubjectEvent: async ({ request, locals: { supabase } }) => {
     const form = await superValidate(request, zod(addSubjectSchema));
-
+    console.log(form.data);
     if (!form.valid) {
       return fail(400, { form });
     }
+
+    const { error } = await supabase.from('subjects_tb').insert([
+      {
+        name: form.data.subjectName,
+        code: form.data.subjectCode,
+        unit: form.data.unit,
+        department: form.data.department
+      }
+    ]);
+    if (error) return fail(401, { form, msg: error.message });
+    return { form, msg: 'Added successfully' };
   },
   updateSubjectEvent: async ({ request, locals: { supabase } }) => {
     const form = await superValidate(request, zod(updateSubjectSchema));
