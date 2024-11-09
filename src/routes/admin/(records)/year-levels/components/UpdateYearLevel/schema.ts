8ec8764f140e44a3e95e926adff1fb31f052ef5b
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-09 11:59:13 +0800

    update: update schema

--- src/routes/admin/(records)/year-levels/components/UpdateYearLevel/schema.ts
+++ src/routes/admin/(records)/year-levels/components/UpdateYearLevel/schema.ts
@@ -1,8 +1,12 @@
+import { departments } from '$lib/metadata';
 import { z } from 'zod';
 
 export const updateYearLevelSchema = z.object({
   id: z.number(),
-  yearLevel: z.string().min(1, { message: 'Must enter year level.' })
+  yearLevel: z.string().min(1, { message: 'Must enter year level.' }),
+  department: z.string().refine((v) => departments.find((d) => d.value === v), {
+    message: 'Department is required'
+  })
 });
 
 export type UpdateYearLevelSchema = typeof updateYearLevelSchema;
