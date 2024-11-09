e191f5a7966e97ab1c8e6754fb3f236a2f4d881d
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-09 08:56:52 +0800

    add cleanup

--- src/routes/admin/class-schedules/components/AddSchedule/AddSchedule.svelte
+++ src/routes/admin/class-schedules/components/AddSchedule/AddSchedule.svelte
@@ -100,6 +100,16 @@
       onclick={() => {
         open = false;
         form.reset();
+        subjects = [
+          {
+            id: crypto.randomUUID(),
+            name: '',
+            startTime: '',
+            endTime: '',
+            day: '',
+            room: ''
+          }
+        ];
       }}
       class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
     >
