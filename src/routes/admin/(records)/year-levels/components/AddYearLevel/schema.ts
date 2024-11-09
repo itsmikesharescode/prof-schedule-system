d48c813bb12333766d2257d0a52d7b09492fda6f
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-09 11:41:35 +0800

    add: update schema

--- src/routes/admin/(records)/year-levels/components/AddYearLevel/schema.ts
+++ src/routes/admin/(records)/year-levels/components/AddYearLevel/schema.ts
@@ -1,7 +1,11 @@
+import { departments } from '$lib/metadata';
 import { z } from 'zod';
 
 export const addYearLevelSchema = z.object({
-  yearLevel: z.string().min(1, { message: 'Must enter year level.' })
+  yearLevel: z.string().min(1, { message: 'Must enter year level.' }),
+  department: z.string().refine((v) => departments.find((d) => d.value === v), {
+    message: 'Department is required'
+  })
 });
 
 export type AddYearLevelSchema = typeof addYearLevelSchema;
