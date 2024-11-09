14938053be089103a082da0bfb7839b770f1adef
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-09 22:46:36 +0800

    update: add subjects and year-levels page filters

--- src/routes/admin/(db_calls)/streamSubjects.ts
+++ src/routes/admin/(db_calls)/streamSubjects.ts
@@ -5,8 +5,24 @@ interface PromiseTypes {
   Subject: Database['public']['Tables']['subjects_tb']['Row'];
 }
 
-export const streamSubjects = async (supabase: SupabaseClient | undefined) => {
+export const streamSubjects = async (
+  supabase: SupabaseClient | undefined,
+  params: string | null
+) => {
   if (!supabase) return null;
+  if (params) {
+    const { data, error } = (await supabase
+      .from('subjects_tb')
+      .select('*')
+      .order('created_at', {
+        ascending: true
+      })
+      .eq('department', params)) as PostgrestSingleResponse<PromiseTypes['Subject'][]>;
+
+    if (error) return null;
+    return data;
+  }
+
   const { data, error } = (await supabase.from('subjects_tb').select('*').order('created_at', {
     ascending: true
   })) as PostgrestSingleResponse<PromiseTypes['Subject'][]>;
--- src/routes/admin/(db_calls)/streamYearLevels.ts
+++ src/routes/admin/(db_calls)/streamYearLevels.ts
@@ -5,8 +5,24 @@ interface PromiseTypes {
   YearLevels: Database['public']['Tables']['year_levels_tb']['Row'];
 }
 
-export const streamYearLevels = async (supabase: SupabaseClient | undefined) => {
+export const streamYearLevels = async (
+  supabase: SupabaseClient | undefined,
+  params: string | null
+) => {
   if (!supabase) return null;
+  if (params) {
+    const { data, error } = (await supabase
+      .from('year_levels_tb')
+      .select('*')
+      .order('created_at', {
+        ascending: true
+      })
+      .eq('department', params)) as PostgrestSingleResponse<PromiseTypes['YearLevels'][]>;
+
+    if (error) return null;
+    return data;
+  }
+
   const { data, error } = (await supabase.from('year_levels_tb').select('*').order('created_at', {
     ascending: true
   })) as PostgrestSingleResponse<PromiseTypes['YearLevels'][]>;
