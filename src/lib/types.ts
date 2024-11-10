770078d90b41f63ac384df9a1a227886e03ac5d2
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-10 13:18:10 +0800

    update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,3 +3,26 @@ export type Result<T> = {
   type: string;
   data: T;
 };
+
+export type UserMetaData = {
+  role: 'Professor';
+  email: string;
+  avatar: string;
+  approved: boolean;
+  lastName: string;
+  firstName: string;
+  interests: string[];
+  department: string;
+  middleName: string;
+  preferredSchedule: {
+    day: string;
+    endTime: string;
+    available: 'Full Time' | 'Part Time';
+    startTime: string;
+  };
+};
+
+export type YearLevel = {
+  id: string;
+  yearLevel: string;
+};
