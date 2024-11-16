c4f0bfef4b8e1ba0d5ce9b80bc019ab9e6bd7682
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-17 00:29:02 +0800

    to be continue

--- src/routes/admin/class-schedules/+page.svelte
+++ src/routes/admin/class-schedules/+page.svelte
@@ -1,70 +1,34 @@
 <script lang="ts">
-  import * as Table from '$lib/components/ui/table/index.js';
-  import FilterPicker from '$lib/components/general/FilterPicker.svelte';
-  import { departments } from '$lib/metadata';
-  import { page } from '$app/stores';
   import { Skeleton } from '$lib/components/ui/skeleton/index';
-  import TableMenu from './components/TableMenu.svelte';
+  import Table from './components/Table/components/Table.svelte';
+  import { columns } from './components/Table/components/columns';
+
   import AddSchedule from './components/AddSchedule/AddSchedule.svelte';
 
   const { data } = $props();
-
-  const detectURL = $derived($page.url.searchParams.get('filter'));
-
-  $effect(() => {
-    if (detectURL) {
-      //stream the filter here if there is no filter gather all rows, else filter it
-    }
-  });
 </script>
 
-<div class="flex flex-col gap-4">
-  <div class="sticky top-2 z-30 flex justify-end">
-    <AddSchedule addScheduleForm={data.addScheduleForm} />
-  </div>
+<div class="sticky top-2 z-30 flex justify-end">
+  <AddSchedule addScheduleForm={data.addScheduleForm} />
+</div>
 
-  <Table.Root>
-    <Table.Caption>A list of your recent invoices.</Table.Caption>
-    <Table.Header>
-      <Table.Row>
-        <Table.Head class="w-[50px]"></Table.Head>
-        <Table.Head class="w-[100px] truncate">Subject Code</Table.Head>
-        <Table.Head class="truncate">Subject Name</Table.Head>
-        <Table.Head class="truncate">Unit</Table.Head>
-        <Table.Head class="truncate">Day</Table.Head>
-        <Table.Head class="truncate">Time</Table.Head>
-        <Table.Head class="truncate">Room</Table.Head>
-      </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      <!--Display if streaming data-->
-      {#if false}
-        {#each Array(5) as _}
-          <Table.Row>
-            <Table.Cell class="">
-              <Skeleton class="h-[20px] rounded-full" />
-            </Table.Cell>
-            <Table.Cell class="font-medium"><Skeleton class="h-[20px] rounded-full" /></Table.Cell>
-            <Table.Cell><Skeleton class="h-[20px] rounded-full" /></Table.Cell>
-            <Table.Cell><Skeleton class="h-[20px] rounded-full" /></Table.Cell>
-            <Table.Cell class="text-right"><Skeleton class="h-[20px] rounded-full" /></Table.Cell>
-          </Table.Row>
-        {/each}
-      {/if}
-
-      {#each Array(20) as _}
-        <Table.Row>
-          <Table.Cell class="">
-            <TableMenu updateScheduleForm={data.updateScheduleForm} />
-          </Table.Cell>
-          <Table.Cell class="font-medium">ComEng101</Table.Cell>
-          <Table.Cell>Computer Engineering</Table.Cell>
-          <Table.Cell>3</Table.Cell>
-          <Table.Cell class="">Monday</Table.Cell>
-          <Table.Cell class="">8:00 AM - 10:00 PM</Table.Cell>
-          <Table.Cell class="">ROOM 1</Table.Cell>
-        </Table.Row>
-      {/each}
-    </Table.Body>
-  </Table.Root>
+<div class="mt-6">
+  {#await data.streamDepartments}
+    <section class="flex flex-col gap-1.5">
+      <Skeleton class="h-[20px] w-[100%] rounded-full" />
+      <Skeleton class="h-[20px] w-[80%] rounded-full" />
+      <Skeleton class="h-[20px] w-[30%] rounded-full" />
+      <Skeleton class="h-[20px] w-[90%] rounded-full" />
+    </section>
+  {:then professors}
+    <!--  <Table
+      data={professors?.map((prof) => ({
+        created_at: prof.created_at,
+        user_id: prof.user_id,
+        fullName: `${prof.user_meta_data.lastName}, ${prof.user_meta_data.firstName} ${prof.user_meta_data.middleName}`,
+        ...prof.user_meta _data
+      })) ?? []}
+      {columns}
+    /> -->
+  {/await}
 </div>
---
+++ src/routes/admin/class-schedules/components/Table/components/TableSubjectsCell.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" module>
+  type TData = unknown;
+</script>
+
+<script lang="ts" generics="TData">
+  import type { Row } from '@tanstack/table-core';
+  import { type ClassSchedulesPageTable } from '../data/schemas';
+  import * as Popover from '$lib/components/ui/popover/index.js';
+  import { Badge } from '$lib/components/ui/badge/index';
+  let { row }: { row: Row<ClassSchedulesPageTable> } = $props();
+</script>
+
+<Popover.Root>
+  <Popover.Trigger class="underline">View</Popover.Trigger>
+  <Popover.Content>
+    <div class="flex flex-wrap gap-1.5">
+      {#each row.original.subjects as subject}
+        <Badge><span class="text-xs font-medium">{subject.name}</span></Badge>
+      {/each}
+    </div>
+  </Popover.Content>
+</Popover.Root>
