3ba3ca98f2815b62b9dfcb0f0f724ac1ae112f28
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-17 11:12:29 +0800

    remove unused code

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import FilterPicker from '$lib/components/general/FilterPicker.svelte';
   import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-  import { departments } from '$lib/metadata';
   import { auxiliaryState } from '$lib/runes/auxiliaryState.svelte';
   import AdminSidebar from './(components)/Nav/AdminSidebar.svelte';
 
@@ -38,16 +35,6 @@
       <Sidebar.Trigger title="Open sidebar" />
     </div>
 
-    <!-- {#if !['/admin/reports', '/admin/logs'].includes($page.url.pathname)}
-      <div class="pointer-events-auto fixed bottom-3 z-50">
-        <FilterPicker
-          streamDepartments={data.streamDepartments}
-          name="Select filter"
-          class="pointer-events-auto top-6 max-w-fit"
-          selections={departments}
-        />
-      </div>
-    {/if} -->
     {@render children()}
   </main>
 </Sidebar.Provider>
