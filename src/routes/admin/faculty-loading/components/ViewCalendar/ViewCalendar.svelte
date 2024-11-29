40dece948786e31eb6eb06a628e1d27bb5a9f4d1
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-29 22:26:02 +0800

    bump version

--- src/routes/admin/faculty-loading/components/ViewCalendar/ViewCalendar.svelte
+++ src/routes/admin/faculty-loading/components/ViewCalendar/ViewCalendar.svelte
@@ -29,14 +29,13 @@
         tableState.setShowViewCalendar(false);
       }}
       class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none"
-    
+    >
       <X class="size-4" />
       <span class="sr-only">Close</span>
     </button>
     <AlertDialog.Header>
       <AlertDialog.Title
         >Viewing Calendar for {tableState.getActiveRow()?.fullName}</AlertDialog.Title
-      >
       >
     </AlertDialog.Header>
 
