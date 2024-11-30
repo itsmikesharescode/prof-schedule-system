47ae83e72cf601629a4c51a24ac638ec5e0119e2
itsmikesharescode <mikejohneviota@gmail.com>
2024-11-30 09:34:42 +0800

    correct nav links

--- src/routes/professor/(components)/nav/Nav.svelte
+++ src/routes/professor/(components)/nav/Nav.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Profile from '../profile/Profile.svelte';
+  import { page } from '$app/stores';
 
   const navItems = [
     {
@@ -19,9 +20,16 @@
 
 <nav class="border-b-2">
   <section class="container mx-auto flex items-center justify-between p-2">
-    <div class="flex items-center gap-2.5">
+    <div class="flex items-center gap-5">
       {#each navItems as item}
-        <a href={item.href}>{item.name}</a>
+        <a
+          href={item.href}
+          class="transition-all
+          {$page.url.pathname === item.href
+            ? 'border-b-[1px] text-muted-foreground'
+            : 'text-primary'}"
+          >{item.name}
+        </a>
       {/each}
     </div>
 
