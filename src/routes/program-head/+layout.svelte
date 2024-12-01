ef661d0ca40536a3a01f81993125c90c495e3303
itsmikesharescode <mikejohneviota@gmail.com>
2024-12-01 12:09:01 +0800

    add program head nav

---
+++ src/routes/program-head/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+  import ProgramHeadSideBar from './(components)/Nav/ProgramHeadSideBar.svelte';
+
+  const { children, data } = $props();
+</script>
+
+<Sidebar.Provider>
+  <ProgramHeadSideBar />
+
+  <section class="w-full min-w-0 p-4">
+    <Sidebar.Trigger />
+    {@render children()}
+  </section>
+</Sidebar.Provider>
