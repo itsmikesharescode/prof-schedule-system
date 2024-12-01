3f86e756484be11a33b462f87d5ce2154cd91220
itsmikesharescode <mikejohneviota@gmail.com>
2024-12-01 20:38:56 +0800

    add correct route

--- src/routes/program-head/(components)/Nav/ProgramHeadSideBar.svelte
+++ src/routes/program-head/(components)/Nav/ProgramHeadSideBar.svelte
@@ -9,19 +9,18 @@
     ChartColumn,
     CalendarRange,
     ChevronDown,
-    CalendarCog,
-    UserRoundCog
+    CalendarCog
   } from 'lucide-svelte';
 
   import * as Sidebar from '$lib/components/ui/sidebar/index.js';
   import { page } from '$app/stores';
-  import Profile from './components/Profile.svelte';
 
-  import Logout from './components/Logout.svelte';
+  import Profile from './components/Profile.svelte';
+  import * as Collapsible from '$lib/components/ui/collapsible';
   const site_map = [
     {
       title: 'Faculty Loading',
-      url: '/program-head/',
+      url: '/program-head/faculty-loading',
       icon: ListRestart
     },
     {
@@ -33,11 +32,34 @@
       title: 'Requests',
       url: '/program-head/requests',
       icon: CalendarRange
+    }
+  ];
+
+  const records_map = [
+    {
+      title: 'School Year',
+      url: '/program-head/school-year',
+      icon: CalendarCog
     },
     {
-      title: 'Profile',
-      url: '/program-head/profile',
-      icon: UserRoundCog
+      title: 'Year Levels',
+      url: '/program-head/year-levels',
+      icon: CalendarCog
+    },
+    {
+      title: 'Sections',
+      url: '/program-head/sections',
+      icon: CalendarCog
+    },
+    {
+      title: 'Subjects',
+      url: '/program-head/subjects',
+      icon: CalendarCog
+    },
+    {
+      title: 'Rooms',
+      url: '/program-head/rooms',
+      icon: CalendarCog
     }
   ];
 </script>
@@ -45,7 +67,7 @@
 <Sidebar.Root>
   <Sidebar.Content class="">
     <Sidebar.Group>
-      <Sidebar.GroupLabel>Prgram Head</Sidebar.GroupLabel>
+      <Sidebar.GroupLabel>Program Head</Sidebar.GroupLabel>
       <Sidebar.GroupContent>
         <Sidebar.Menu>
           {#each site_map as site (site.title)}
@@ -65,6 +87,42 @@
         </Sidebar.Menu>
       </Sidebar.GroupContent>
     </Sidebar.Group>
+    <Collapsible.Root open class="group/collapsible">
+      <Sidebar.Group>
+        <Sidebar.GroupLabel>
+          {#snippet child({ props })}
+            <Collapsible.Trigger {...props}>
+              Records
+              <ChevronDown
+                class="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180"
+              />
+            </Collapsible.Trigger>
+          {/snippet}
+        </Sidebar.GroupLabel>
+        <Collapsible.Content>
+          <Sidebar.GroupContent>
+            <Sidebar.Menu>
+              {#each records_map as record (record.title)}
+                <Sidebar.MenuItem
+                  class={record.url === $page.url.pathname
+                    ? 'bg-gray-100 dark:bg-secondary/90'
+                    : ''}
+                >
+                  <Sidebar.MenuButton>
+                    {#snippet child({ props })}
+                      <a href={record.url} {...props}>
+                        <record.icon />
+                        <span>{record.title}</span>
+                      </a>
+                    {/snippet}
+                  </Sidebar.MenuButton>
+                </Sidebar.MenuItem>
+              {/each}
+            </Sidebar.Menu>
+          </Sidebar.GroupContent>
+        </Collapsible.Content>
+      </Sidebar.Group>
+    </Collapsible.Root>
   </Sidebar.Content>
 
   <Sidebar.Footer>
