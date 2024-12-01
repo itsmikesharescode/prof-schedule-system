4d15b788be7d4bad8fdfccf8705cc011fbaffd87
itsmikesharescode <mikejohneviota@gmail.com>
2024-12-01 20:58:51 +0800

    add faculty loading page

---
+++ src/routes/program-head/(index)/components/Table/components/TableFullnameRow.svelte
@@ -0,0 +1,25 @@
+<script lang="ts" module>
+  type TData = unknown;
+</script>
+
+<script lang="ts" generics="TData">
+  import * as Avatar from '$lib/components/ui/avatar/index.js';
+  import type { Row } from '@tanstack/table-core';
+  import { type FacultyLoadingPageTable } from '../data/schemas';
+  import { PUBLIC_SUPABASE_STORAGE_URL } from '$env/static/public';
+
+  let { row }: { row: Row<FacultyLoadingPageTable> } = $props();
+</script>
+
+<div class="flex items-center gap-2.5">
+  <Avatar.Root>
+    <Avatar.Image
+      src={`${PUBLIC_SUPABASE_STORAGE_URL}${row.original.professor_meta_data?.avatar}`}
+      alt="profile"
+    />
+    <Avatar.Fallback>{row.original.fullName[0].toUpperCase()}</Avatar.Fallback>
+  </Avatar.Root>
+  <div class="">
+    {row.original.fullName}
+  </div>
+</div>
