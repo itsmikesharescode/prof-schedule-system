47e101b36b8391feef2c94173961d99aeb9ea65c
itsmikesharescode <mikejohneviota@gmail.com>
2024-12-01 23:06:05 +0800

    add request page

---
+++ src/routes/program-head/requests/+page.server.ts
@@ -0,0 +1,42 @@
+import type { Actions, PageServerLoad } from './$types';
+import streamRequests from '../(db_calls)/streamRequest';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { updateRequestStatusSchema } from './components/UpdateRequestStatus/schema';
+import { fail } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async ({ locals: { supabase } }) => {
+  return {
+    updateRequestStatusForm: await superValidate(zod(updateRequestStatusSchema)),
+    getRequests: streamRequests(supabase)
+  };
+};
+
+export const actions: Actions = {
+  updateRequestStatusEvent: async ({ locals: { supabase }, request }) => {
+    const form = await superValidate(request, zod(updateRequestStatusSchema));
+    if (!form.valid) {
+      return fail(400, { form });
+    }
+
+    const { error } = await supabase
+      .from('requests_tb')
+      .update({
+        status: form.data.status
+      })
+      .eq('id', form.data.id);
+
+    if (error) return fail(401, { form, msg: error.message });
+
+    return { form, msg: 'Request status updated successfully' };
+  },
+  deleteRequestEvent: async ({ locals: { supabase }, request }) => {
+    const formData = await request.formData();
+    const id = formData.get('id') as string;
+
+    const { error } = await supabase.from('requests_tb').delete().eq('id', Number(id));
+
+    if (error) return fail(401, { msg: error.message });
+    return { msg: 'Request deleted successfully' };
+  }
+};
---
+++ src/routes/program-head/requests/components/Table/components/index.ts
@@ -0,0 +1,10 @@
+export { default as TableCheckbox } from './TableCheckbox.svelte';
+export { default as TableTitleCell } from './TableTitleCell.svelte';
+export { default as TableRowActions } from './TableRowAction.svelte';
+export { default as TableColumnHeader } from './ColumnHeader.svelte';
+export { default as TableToolbar } from './TableToolbar.svelte';
+export { default as TablePagination } from './TablePagination.svelte';
+export { default as TableViewOptions } from './TableViewOptions.svelte';
+export { default as TableFacetedFilter } from './TableFacetedFilter.svelte';
+export { default as TableSubjectsCell } from './TableSubjectsCell.svelte';
+export { default as TableFullnameRow } from './TableFullnameRow.svelte';
