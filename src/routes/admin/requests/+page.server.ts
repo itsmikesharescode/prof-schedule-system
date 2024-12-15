86b75161a100befb6ca239e80cc0d8c2a1732fab
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-15 10:43:09 +0800

    add request func

--- src/routes/admin/requests/+page.server.ts
+++ src/routes/admin/requests/+page.server.ts
@@ -6,37 +6,48 @@ import { updateRequestStatusSchema } from './components/UpdateRequestStatus/sche
 import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
-  return {
-    updateRequestStatusForm: await superValidate(zod(updateRequestStatusSchema)),
-    getRequests: streamRequests(supabase)
-  };
+	return {
+		updateRequestStatusForm: await superValidate(zod(updateRequestStatusSchema)),
+		getRequests: streamRequests(supabase)
+	};
 };
 
 export const actions: Actions = {
-  updateRequestStatusEvent: async ({ locals: { supabase }, request }) => {
-    const form = await superValidate(request, zod(updateRequestStatusSchema));
-    if (!form.valid) {
-      return fail(400, { form });
-    }
+	updateRequestStatusEvent: async ({ locals: { supabaseAdmin }, request }) => {
+		const form = await superValidate(request, zod(updateRequestStatusSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
 
-    const { error } = await supabase
-      .from('requests_tb')
-      .update({
-        status: form.data.status
-      })
-      .eq('id', form.data.id);
+		const { error } = await supabaseAdmin
+			.from('requests_tb')
+			.update({
+				status: form.data.status
+			})
+			.eq('id', form.data.id);
 
-    if (error) return fail(401, { form, msg: error.message });
+		if (error) return fail(401, { form, msg: error.message });
 
-    return { form, msg: 'Request status updated successfully' };
-  },
-  deleteRequestEvent: async ({ locals: { supabase }, request }) => {
-    const formData = await request.formData();
-    const id = formData.get('id') as string;
+		const { error: updateUserErr } = await supabaseAdmin.auth.admin.updateUserById(
+			form.data.user_id,
+			{
+				user_metadata: {
+					schedule: form.data.newSched
+				}
+			}
+		);
 
-    const { error } = await supabase.from('requests_tb').delete().eq('id', Number(id));
+		if (updateUserErr) return fail(401, { form, msg: updateUserErr.message });
 
-    if (error) return fail(401, { msg: error.message });
-    return { msg: 'Request deleted successfully' };
-  }
+		return { form, msg: 'Request approved successfully' };
+	},
+	deleteRequestEvent: async ({ locals: { supabase }, request }) => {
+		const formData = await request.formData();
+		const id = formData.get('id') as string;
+
+		const { error } = await supabase.from('requests_tb').delete().eq('id', Number(id));
+
+		if (error) return fail(401, { msg: error.message });
+		return { msg: 'Request deleted successfully' };
+	}
 };
