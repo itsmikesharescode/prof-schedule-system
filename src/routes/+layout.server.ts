d9842a11d291ee727f51fea22fd9cb0232955328
Mike John Eviota <eviotamikejohnb@gmail.com>
2024-12-18 22:27:04 +0800

    add download room UI

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,27 +2,27 @@ import type { LayoutServerLoad } from './$types';
 import { streamDepartments } from './(auxiliary_calls)/streamDepartments';
 
 export const load: LayoutServerLoad = async ({
-  locals: { safeGetSession, supabase, supabaseAdmin },
-  cookies
+	locals: { safeGetSession, supabase, supabaseAdmin },
+	cookies
 }) => {
-  const { session } = await safeGetSession();
+	const { session } = await safeGetSession();
 
-  /* const { data, error } = await supabaseAdmin.auth.admin.updateUserById(
-    '9da98af9-7dbe-4e41-b25c-185af1235ba6',
-    {
-      user_metadata: {
-        role: 'admin',
-        email: 'localadmin@gmail.com',
-        firstName: 'Mike',
-        middleName: 'Local',
-        lastName: 'Admin'
-      }
-    }
-  );
-  console.log(data, error?.message); */
-  return {
-    session,
-    cookies: cookies.getAll(),
-    streamDepartments: streamDepartments(supabase)
-  };
+	/* const { data, error } = await supabaseAdmin.auth.admin.updateUserById(
+		'9de8f786-c84d-4cbb-b345-3199e55213c5',
+		{
+			user_metadata: {
+				role: 'admin',
+				email: 'localadmin@gmail.com',
+				firstName: 'Mike',
+				middleName: 'Local',
+				lastName: 'Admin'
+			}
+		}
+	);
+	console.log(data, error?.message); */
+	return {
+		session,
+		cookies: cookies.getAll(),
+		streamDepartments: streamDepartments(supabase)
+	};
 };
---
+++ src/routes/admin/class-schedules/components/Table/components/PrintRooms.svelte
@@ -0,0 +1,157 @@
+<script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import PrinterCheck from 'lucide-svelte/icons/printer-check';
+	import { buttonVariants } from '$lib/components/ui/button';
+	import ChevronDown from 'lucide-svelte/icons/chevron-down';
+	import Check from 'lucide-svelte/icons/check';
+	import ChevronsUpDown from 'lucide-svelte/icons/chevrons-up-down';
+	import Download from 'lucide-svelte/icons/download';
+	import { tick } from 'svelte';
+	import * as Command from '$lib/components/ui/command/index.js';
+
+	const rooms = [
+		{
+			value: 'Room 101',
+			label: 'Room 101'
+		},
+		{
+			value: 'Room 102',
+			label: 'Room 102'
+		},
+		{
+			value: 'Room 103',
+			label: 'Room 103'
+		},
+		{
+			value: 'Room 104',
+			label: 'Room 104'
+		},
+		{
+			value: 'Room 105',
+			label: 'Room 105'
+		},
+		{
+			value: 'Room 106',
+			label: 'Room 106'
+		},
+		{
+			value: 'Room 107',
+			label: 'Room 107'
+		},
+		{
+			value: 'Room 108',
+			label: 'Room 108'
+		},
+		{
+			value: 'Room 109',
+			label: 'Room 109'
+		},
+		{
+			value: 'Room 110',
+			label: 'Room 110'
+		}
+	];
+
+	let value = $state('');
+	let triggerRef = $state<HTMLButtonElement>(null!);
+
+	class SelectedRoomState {
+		#selectedItems = $state<typeof rooms>([]);
+
+		add(roomValue: (typeof rooms)[number]) {
+			const deepCopy = this.#selectedItems.map((item) => item.value);
+			if (deepCopy.includes(roomValue.value)) {
+				this.#selectedItems = this.#selectedItems.filter((item) => item.value !== roomValue.value);
+				return;
+			}
+
+			this.#selectedItems.push(roomValue);
+		}
+
+		get() {
+			return this.#selectedItems;
+		}
+
+		checkChecks(value: string) {
+			const deepCopy = this.#selectedItems.map((item) => item.value);
+			if (deepCopy.includes(value)) {
+				return '';
+			}
+
+			return 'text-transparent';
+		}
+	}
+
+	const selectedValue = $derived(rooms.find((f) => f.value === value)?.label);
+
+	const selectedRoomState = new SelectedRoomState();
+
+	const checkChecks = (room: string) => {
+		const deepCopy = selectedItems.map((item) => item.value);
+
+		if (deepCopy.includes(room)) {
+			return '';
+		}
+
+		return 'text-transparent';
+	};
+
+	function closeAndFocusTrigger() {
+		open = false;
+		tick().then(() => {
+			triggerRef.focus();
+		});
+	}
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger class={buttonVariants({ variant: 'outline', size: 'sm' })}>
+		Download Rooms <Download />
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Group>
+			<DropdownMenu.GroupHeading>Choose option</DropdownMenu.GroupHeading>
+			<DropdownMenu.Separator />
+			<DropdownMenu.Sub>
+				<DropdownMenu.SubTrigger>
+					<span>Download Specific Room</span>
+				</DropdownMenu.SubTrigger>
+				<DropdownMenu.SubContent sideOffset={10} class="mt-20">
+					<Command.Root>
+						<Command.Input placeholder="Search room..." />
+						<Command.List>
+							<Command.Empty>No room found</Command.Empty>
+							<Command.Group>
+								{#each rooms as room}
+									<Command.Item
+										value={room.value}
+										onSelect={() => {
+											value = room.value;
+											selectedRoomState.add(room);
+
+											closeAndFocusTrigger();
+										}}
+									>
+										<Check class={selectedRoomState.checkChecks(room.value)} />
+										{room.label}
+									</Command.Item>
+								{/each}
+							</Command.Group>
+						</Command.List>
+					</Command.Root>
+					{#if selectedRoomState.get().length}
+						<DropdownMenu.Item class="mt-5 ">
+							<Download />
+							<span>Download Selections</span>
+						</DropdownMenu.Item>
+					{/if}
+				</DropdownMenu.SubContent>
+			</DropdownMenu.Sub>
+
+			<DropdownMenu.Item>
+				<Download />
+				Download All
+			</DropdownMenu.Item>
+		</DropdownMenu.Group>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
